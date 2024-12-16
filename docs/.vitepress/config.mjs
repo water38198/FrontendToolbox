6b414f269ca1085671cf8d25a6ca48a32cdeb6ef
PayRoom <water38198@gmail.com>
2024-12-16 21:48:45 +0800

    init

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "前端法師的工具箱",
+  description: "A blog with some awesome frontend tricks",
+  head: [],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首頁', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '文章', link: '/posts/' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/posts/index.md
@@ -0,0 +1,4 @@
+# Articles
+
+[回首頁](../index)
+[去Google](https://www.google.com.tw/?hl=zh_TW)
